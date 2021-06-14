2c60b8fcb940466f9c5e53cb84c3317909108433
dimitry <slepacusanimation@gmail.com>
2021-06-14 17:29:38 +0100

    first

--- tasker/tasker.py
+++ tasker/tasker.py
@@ -1,4 +1,15 @@
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
+from kivymd.app import MDApp
 
-print('something on the output')
-print('sdsdfgsfg')
+class ContentNavigationDrawer(BoxLayout):
+    screen_manager = ObjectProperty()
+    new_drawer= ObjectProperty()
 
+class Test(MDApp):
+    def build(self):
+        return Builder.load_file('tasker.kv')
+
+
+Test().run()
