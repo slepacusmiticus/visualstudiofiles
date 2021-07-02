079ea3b6e60952bf9297830285affa47f8ecda99
dimitry <slepacusanimation@gmail.com>
2021-07-02 17:30:14 +0100

    end part 3

--- pygame/sprite/pygame_fnc/platformer/main.py
+++ pygame/sprite/pygame_fnc/platformer/main.py
@@ -4,22 +4,27 @@ from settings import *
 from sprites import *
 
 class Game:
-    def __init__(self):
-        
-
+    def __init__(self):   
+        #initialaize game wndw, etc
         pg.init()
         pg.mixer.init()
         self.screen=pg.display.set_mode((WIDTH, HEIGHT))
-        pg.display.set_caption('Platformer')
+        pg.display.set_caption(TITLE)
         self.clock =pg.time.Clock()
         self.running = True
 
     def new(self):
         #start a new game
-
         self.all_sprites = pg.sprite.Group()
+        self.platforms= pg.sprite.Group()
         self.player = Player()
         self.all_sprites.add(self.player)
+        p1=Platform(0,HEIGHT-40, WIDTH,40)
+        self.all_sprites.add(p1)
+        self.platforms.add(p1)
+        p2=Platform(WIDTH/2-50, HEIGHT*3/4, 100, 20)
+        self.all_sprites.add(p2)
+        self.platforms.add(p2)
         self.run()
     
     def run(self):
@@ -31,11 +36,16 @@ class Game:
             self.events()
             self.update()
             self.draw()
+           
 
 
     def update(self):
         #game loop update
         self.all_sprites.update()
+        hits= pg.sprite.spritecollide(self.player,self.platforms,False)
+        if hits:
+            self.player.pos.y =hits[0].rect.top
+            self.player.vel.y =0
 
     def events(self):
             
--- pygame/sprite/pygame_fnc/platformer/sprites.py
+++ pygame/sprite/pygame_fnc/platformer/sprites.py
@@ -14,14 +14,15 @@ class Player(pg.sprite.Sprite):
         self.acc=vec(0,0)
 
     def update(self):
-        self.acc=vec(0,0)
+        self.acc=vec(0,0.5)
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT]:
             self.acc.x = -PLAYER_ACC
         if keys[pg.K_RIGHT]:
             self.acc.x = PLAYER_ACC
 
-        self.acc += self.vel *PLAYER_FRICTION
+        #apply friction
+        self.acc.x += self.vel.x * PLAYER_FRICTION
         self.vel += self.acc
         self.pos += self.vel +0.5*self.acc
 
@@ -30,4 +31,13 @@ class Player(pg.sprite.Sprite):
         if self.pos.x<0:
             self.pos.x =WIDTH
 
-        self.rect.center=self.pos
+        self.rect.midbottom=self.pos
+
+class Platform(pg.sprite.Sprite):
+    def __init__(self,x,y,w,h):
+        pg.sprite.Sprite.__init__(self)
+        self.image= pg.Surface((w,h))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
